f9e826ee064d550081ec8b183bffac2681aceae7
orjanen <superszura@gmail.com>
2021-04-01 17:00:46 +0200

    added react router and a homePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import React, {Fragment} from 'react';
+import {Container} from "semantic-ui-react";
+import { Route, Switch } from 'react-router-dom';
+
+import HomePage from "./pages/HomePage";
 
 const App = () => {
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          test starter <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Fragment>
+        <Route path='/' exact component={HomePage}/>
+        <Route path={'/(.+)'} render={() => (
+            <Fragment>
+              <Container style={{marginTop: '7em'}}>
+                <Switch>
+                </Switch>
+              </Container>
+            </Fragment>
+        )}/>
+      </Fragment>
   );
 }
 
